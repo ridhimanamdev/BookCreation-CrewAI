a4c2cd2c8fc22322f2b5afa2f4b741b04e6ebcc6
ridhimanamdev <ridhimanamdev84145@gmail.com>
2025-08-20 22:48:22 +0530

    Main py updates

--- my_book/src/my_book/main.py
+++ my_book/src/my_book/main.py
@@ -29,7 +29,11 @@ class BookFlow(BookState):
         Start the book writing flow by generating the book outline.
         """
         print("Kick off the Book Outline Crew and Generating book outline...")
-        output=(OutlineCrew(), crew(), kickoff(input={"topic": self.topic, "goal": self.goal}))
+        output=(
+            OutlineCrew()
+            ,crew()
+            ,kickoff(input={"topic": self.topic, "goal": self.goal})
+        )
 
 
         chapter = output["chapter"]
@@ -41,4 +45,74 @@ class BookFlow(BookState):
     @listen(generate_book_outline)
     async def write_chapters(self):
         print("Kick off the WriteBookChapterCrew and Writing book chapters...")
-    
+        tasks = []
+
+        async def write_single_chapter(chapter_outline):
+            output = (
+                WriteBookChapterCrew(), 
+                crew(),
+                kickoff(input=
+                        {
+                            "goal": self.state.goal,
+                            "topic": self.state.topic,
+                            "chapter_title": chapter_outline.title,
+                            "chapter_description": chapter_outline.description,
+                            "book_outline": [
+                                chapter_outline.model_dump_json()
+                                for chapter_outline in self.state.book_outline
+                            ],
+                        }
+                    ) 
+                )
+            title = output["title"]
+            content= output["content"]
+            chapter = Chapter(title=title, content=content)
+            return chapter
+        
+        for chapter_outline in self.state.book_outline:
+            print(f"Writing chapter: {chapter_outline.title}")
+            print(f"Chapter description: {chapter_outline.description}")
+            #Schedule the writing of each chapter
+
+            task = asyncio.create_task(write_single_chapter(chapter_outline))
+            tasks.append(task)
+
+        #await for all chapter writing tasks to complete
+        chapters = await asyncio.gather(*tasks)
+        print("Newly generated chapters:", chapters)
+        self.state.book.extend(chapters)
+
+        print ("Book Chapters:", self.state.book)
+
+    @listen(write_chapters)
+    async def join_and_save_chapter(self):
+        print("Joining and saving chapters...")
+        # Here you can implement the logic to join and save the chapters
+        # For example, you can save them to a file or database
+        book_content = ""
+
+        for chapter in self.state.book:
+            book_content += f"Chapter: {chapter.title}\n"
+            book_content += f"Content: {chapter.content}\n\n"
+
+        book_title =self.state.title
+        filename = f"{book_title.replace(' ', '_')}.md"
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(book_content)
+        print(f"Book saved as {filename}")
+        return book_content
+
+def kickoff(input: dict):
+    """
+    Kick off the flow with the provided input.
+    """
+    poem_flow = BookFlow()
+    poem_flow.kickoff()
+
+def plot():
+    poem_flow = BookFlow()
+    poem_flow.plot()
+
+if __name__ == "__main__":
+    kickoff()
+
